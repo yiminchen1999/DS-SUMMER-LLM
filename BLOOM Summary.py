af2df8580b387069308728eff0b425844703f3fd
ÿiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2023-06-06 21:49:27 -0400

    finetune

--- BLOOM Summary.py
+++ BLOOM Summary.py
@@ -1,12 +1,26 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
+import torch
+
+language_model = "bigscience/bloomz-1b1"
+
+# Enable CUDA if desired
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = AutoModelForCausalLM.from_pretrained(language_model, use_cache=True).to(device)
+tokenizer = AutoTokenizer.from_pretrained(language_model)
+
+# Source text
+prompt = "BLOOMwastrainedontheROOTScorpus(Lauren¸conetal.,2022),acompositecollection of498HuggingFacedatasets(Lhoestetal.,2021)amountingto1.61terabytesoftextthat span46naturallanguagesand13programminglanguages.Ahigh-leveloverviewofthis datasetcanbeseeninFigure3,whileadetaileditemizedlistofeverylanguagealong withitslinguisticgenus,familyandmacroareaispresentedinTable1.Beyondthecorpus itself,theprocessresultedinthedevelopmentandreleaseofanumberoforganizational andtechnicaltools,includingthoseillustratedinFigure2."
 
 
 summary_instruction = f"\n\nSummarize the previous text in three sentences:\n\n"
 
 total_prompt = prompt + summary_instruction
 
-input_ids = tokenizer(prompt + f"\n\n" + summary_instruction, return_tensors="pt").to(0)
-sample = model.generate(**input_ids, max_length=5000,  top_k=1, temperature=0.9, repetition_penalty = 2.0)
+input_ids = tokenizer.encode(total_prompt, return_tensors="pt").to(device)
+sample = model.generate(input_ids, max_length=5000, top_k=1, temperature=0.9, repetition_penalty=2.0)
 
-result_string = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'", "\n\n\n"])
+result_string = tokenizer.decode(sample[0], skip_special_tokens=True)
 
-print(result_string[len(total_prompt)::])
+print(result_string)
+#summary = "A new database has been created to help researchers understand how humans communicate across languages"
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# DS-SUMMER-LLM
+## DS-SUMMER-LLM
 # finetuned BLOOM to produce [BLOOMZ] & our guide is here[https://github.com/bigscience-workshop/xmtf#bloomz].
+# [https://huggingface.co/bigscience/bloomz]
+# paper "BLOOM: A 176B-Parameter Open-Access Multilingual Language Model" [https://arxiv.org/abs/2211.05100]
